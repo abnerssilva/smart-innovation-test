3c46beccd102aed2cbab4ad1e31d9c77fed33251
Abner Souza <abner.souza@roit.com.br>
2021-12-18 23:23:20 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// if (window.confirm(`Esse teste foi construído para a proporção 1900x980 e não é responsivo.\n
+// Se sua tela não tiver essa proporção (Monitor ou TV 32"), clique em OK e veja um vídeo com instruções para visualizá-lo corretamente.\n
+// Espero que gostem!! ;)
+// `)) 
+// {
+// window.open('https://www.google.com/chrome/browser/index.html', '_blank');
+// };
